be998dec36c895674e737be2041a9bbcc287982f
Sartik0709 <sharmasartik@gmail.com>
2024-07-25 01:34:36 +0530

    portfolio

---
+++ src/components/About/AboutCard.js
@@ -0,0 +1,43 @@
+import React from "react";
+import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+
+function AboutCard() {
+  return (
+    <Card className="quote-card-view">
+      <Card.Body>
+        <blockquote className="blockquote mb-0">
+          <p style={{ textAlign: "justify" }}>
+            Hi Everyone, I am <span className="purple">Sartik Sharma </span>
+            from <span className="purple"> Sirsa, India.</span>
+            <br />
+            I am Full-Stack WebDeveloper
+            <br />
+            I have completed Bachelor of Technology in Computer Science and Engineering at JCDV Sirsa 
+            <br />
+            <br />
+            Apart from coding, some other activities that I love to do!
+          </p>
+          <ul>
+            <li className="about-activity">
+              <ImPointRight /> Playing Football
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Hiking
+            </li>
+            <li className="about-activity">
+              <ImPointRight /> Travelling
+            </li>
+          </ul>
+
+          <p style={{ color: "rgb(155 126 172)" }}>
+            "Strive to build things that make a difference!"{" "}
+          </p>
+          <footer className="blockquote-footer">Sartik</footer>
+        </blockquote>
+      </Card.Body>
+    </Card>
+  );
+}
+
+export default AboutCard;
